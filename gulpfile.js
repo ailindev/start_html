b9827eac30dba67e942ccf6feda3e034f84920c2
Andrew Ilin <aniki.iljin@gmail.com>
2019-11-02 04:37:52 +0500

    upgrade

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,10 @@ let gulp = require('gulp'),
     rename = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer'),
     notify = require('gulp-notify'),
-    rsync = require('gulp-rsync');
+    rsync = require('gulp-rsync'),
+    imagemin = require('gulp-imagemin'),
+    mozjpeg = require('imagemin-mozjpeg'),
+    del = require('del');
 
 gulp.task('browser-sync', function () {
    browserSync({
@@ -24,7 +27,7 @@ gulp.task('browser-sync', function () {
 });
 
 gulp.task('styles', function () {
-   return gulp.src('app/scss/**/*.scss')
+   return gulp.src('app/scss/style.scss')
        .pipe(sass({outputStyle: 'expanded'}).on("error", notify.onError()))
        .pipe(rename({suffix: '.min', prefix: ''}))
        .pipe(autoprefixer(['last 15 versions']))
@@ -35,9 +38,9 @@ gulp.task('styles', function () {
 
 gulp.task('scripts', function () {
    return gulp.src([
-      'app/libs/jquery/dist/jquery.min.js',
-      //'app/libs/bootstrap/dist/js/bootstrap.js',
-      'app/js/common.js', // Always at the end
+      'node_modules/jquery/dist/jquery.min.js',
+      //'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+      'app/js/_custom.js', // Always at the end
    ])
        .pipe(concat('scripts.min.js'))
        // .pipe(uglify()) // Minify js (opt.)
@@ -48,6 +51,26 @@ gulp.task('scripts', function () {
 gulp.task('code', function () {
    return gulp.src('app/*.html')
        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('clean', function(){
+   return del(['app/css', 'app/img/**/*', '!app/img/_src'], {force:true});
+});
+
+gulp.task('img:dev', function () {
+   return gulp.src('app/img/_src/**/*.{png,jpg,webp,svg}')
+       .pipe(gulp.dest('app/img'))
+       .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('img:build', function () {
+   return gulp.src('app/img/_src/**/*.{png,jpg,webp,svg}')
+       .pipe(imagemin([
+          mozjpeg({
+             quality: 80
+          })
+       ]))
+       .pipe(gulp.dest('app/img'));
 });
 
 gulp.task('rsync', function () {
@@ -67,7 +90,10 @@ gulp.task('rsync', function () {
 
 gulp.task('watch', function () {
    gulp.watch('app/scss/**/*.scss', gulp.parallel('styles'));
-   gulp.watch(['libs/**/*.js', 'app/js/common.js'], gulp.parallel('scripts'));
+   gulp.watch(['libs/**/*.js', 'app/js/_custom.js'], gulp.parallel('scripts'));
    gulp.watch('app/*.html', gulp.parallel('code'));
+   gulp.watch('app/img/_src/**/*', gulp.parallel('img:dev'));
 });
-gulp.task('default', gulp.parallel('styles', 'scripts', 'browser-sync', 'watch'));
+
+gulp.task('default', gulp.parallel('styles', 'scripts', 'img:dev', 'browser-sync', 'watch'));
+gulp.task('build', gulp.series('clean', 'styles', 'scripts', 'img:build'));
